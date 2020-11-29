009dbe066836cbf96907d746af36c4101f1c4a93
ywarezk <ywarezk@gmail.com>
2020-11-29 16:06:24 +0200

    fixed the phone value accessor

--- 04-advanced-forms/forms-tutorial/src/app/05-custom-form-controls/dash.pipe.ts
+++ 04-advanced-forms/forms-tutorial/src/app/05-custom-form-controls/dash.pipe.ts
@@ -6,12 +6,18 @@ export class DashPipe implements PipeTransform {
   transform(value: string, ...positions: number[] ): any {
     let result = '';
     let index = 0;
+    if (value.length <= positions[0]) return value;
     for(let num of positions) {
+      if (value[index] === '-') index++;
       result += value.substring(index, num);
       result += '-'
       index = num;
     }
-    result+=value.substring(index, value.length);
+    if (value[index] === '-') {
+      result += value.substring(index + 1, value.length);
+    } else {
+      result += value.substring(index, value.length);
+    }
     return result;
   }
 }
